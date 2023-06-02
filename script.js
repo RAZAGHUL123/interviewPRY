fdc5ad8ed60a23d43a8f74a61e5edc3e6e541aba
Rafael <wickedxwayxgaming@gmail.com>
2023-06-01 17:51:26 -0700

    changes

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 // todo, select the right things
-const shrinkBtn = ""
-const expandBtn = ""
-const photo = ""
+//const shrinkBtn = document.querySelector("#shrinkBtn");
+//const expandBtn = ""
+//const photo = ""
+
+//shrinkBtn.addEventListener("click", () => {
+    //photo.style.transform = "scale(0.5)";
+//});
+
+//expandBtn.addEventListener("click", () => {
+    //photo.style.transform = "scale(1)";
+//});
+
+const shrinkBtn = document.querySelector("#shrinkBtn"); // Selecting the shrink button element using its ID
+const expandBtn = document.querySelector("#expandBtn"); // Selecting the expand button element using its ID
+const photo = document.querySelector("#photo"); // Selecting the photo element using its ID
 
 shrinkBtn.addEventListener("click", () => {
-    photo.style.transform = "scale(0.5)";
+    photo.style.transform = "scale(0.5)"; // Applying the scale transformation of 0.5 to shrink the photo
 });
 
 expandBtn.addEventListener("click", () => {
-    photo.style.transform = "scale(1)";
+    photo.style.transform = "scale(1)"; // Applying the scale transformation of 1 to restore the original size of the photo
 });
